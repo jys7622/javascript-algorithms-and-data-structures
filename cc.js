c8df7da0a329c0dc8c0ab8bf6eec3330d37682ad
jys7622 <jys7622@gmail.com>
2021-11-03 09:56:24 +0900

    updated card counting problem

--- cc.js
+++ cc.js
@@ -1,23 +1,48 @@
 let count = 0;
 
+// function cc(card) {
+//   // Only change code below this line
+//   if(card > 1 && card < 7){
+//     count++;
+//   } else if(card == 10 || card == 'J' || card == 'Q' || card == 'K' || card == 'J' || card == 'A'){
+//     count--;
+//   }
+
+//   if(count > 0){
+//     return count + " Bet";
+//   } else {
+//     return count + " Hold";
+//   }
+//   // Only change code above this line
+// }
+
 function cc(card) {
-  // Only change code below this line
-  if(card > 1 && card < 7){
-    count++;
-  } else if(card == 10 || card == 'J' || card == 'Q' || card == 'K' || card == 'J' || card == 'A'){
-    count--;
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+    default:
+      break;
   }
-
-  if(count > 0){
-    return count + " Bet";
+  if (count > 0) {
+    return count + ' Bet';
   } else {
-    return count + " Hold";
-  }  
-  // Only change code above this line
+    return count + ' Hold';
+  }
 }
-
-console.log(cc(2)); 
-console.log(cc(3)); 
-console.log(cc(7)); 
-console.log(cc('K')); 
+console.log(cc(2));
+console.log(cc(3));
+console.log(cc(7));
+console.log(cc('K'));
 console.log(cc('A'));
---
+++ switch_case.js
@@ -0,0 +1,14 @@
+const n = 2;
+switch (n) {
+  case 1:
+    console.log('case 1');
+    // break;
+  case 2:
+    console.log('case 2');
+    // break;
+  case 5:
+    console.log('case 5');
+    // break;
+  default:
+    console.log('case default');
+}
